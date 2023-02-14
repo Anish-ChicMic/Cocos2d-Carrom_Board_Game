120ea06f4830e98f3ac46aaeca3b9b27857aa2e3
Annii <anish.1903.kmr@gmail.com>
2023-02-15 01:41:24 +0530

    Generalised the puchArrangement() function / still need some improvement

--- assets/scripts/gamePlayScript.ts
+++ assets/scripts/gamePlayScript.ts
@@ -11,54 +11,76 @@ export class gamePlayScript extends Component {
     carromCoinPrefab: Prefab | null = null;
 
     onLoad() {
-        this.createPucks(9, PUCK_TYPE.BLACK, new Vec2(20, 30));
-        this.createPucks(9, PUCK_TYPE.WHITE, new Vec2(50, 50));
-        this.createPucks(1, PUCK_TYPE.RED, new Vec2(80, 60));
+        this.createPucks(9, PUCK_TYPE.BLACK);
+        this.createPucks(9, PUCK_TYPE.WHITE);
+        this.createPucks(1, PUCK_TYPE.RED);
 
-        let pucks = this.node.getChildByName("carromBoard").getChildByName("initialCoinsArrangement").children;
+        // let pucks = this.node.getChildByName("carromBoard").getChildByName("initialCoinsArrangement").children;
+        let carromBoardNode = this.node.getChildByName("carromBoard");
+        let pucks = carromBoardNode.children.slice(2, carromBoardNode.children.length);
 
-        this.arrangePucks(pucks);
+        this.arrangePucks(pucks, -44, 40);
+        console.log(this.node.getChildByName("carromBoard").getChildByName("initialCoinsArrangement").children.length);
     }
 
-    start() {}
+    start() { }
 
-    update(deltaTime: number) {}
+    update(deltaTime: number) { }
 
-    createPucks(noOfpucksReq: number, puckType: PUCK_TYPE, pos: Vec2) {
+    createPucks(noOfpucksReq: number, puckType: PUCK_TYPE) {
         for (let no = 0; no < noOfpucksReq; no++) {
             let newCrmcoin = instantiate(this.carromCoinPrefab);
             newCrmcoin.getComponent(crmCoinPrefabScript).setPuck(puckType);
-            newCrmcoin.getComponent(crmCoinPrefabScript).setPuckPosition(pos);
             newCrmcoin.getComponent(UITransform).setContentSize(new Size(20, 20));
-            this.node.getChildByName("carromBoard").getChildByName("initialCoinsArrangement").addChild(newCrmcoin);
+            // this.node.getChildByName("carromBoard").getChildByName("initialCoinsArrangement").addChild(newCrmcoin);
+            this.node.getChildByName("carromBoard").addChild(newCrmcoin);
         }
     }
 
-    arrangePucks(pucks) {
-        let puckLength = pucks.length;
-        let block = this.node.getChildByName("carromBoard").getChildByName("initialCoinsArrangement");
-        let initialPosY = block.getPosition().y + 50;
-        let initialPosX = block.getPosition().x - 50;
-        let initialPosZ = block.getPosition().z;
+    arrangePucks(pucks: object, originX: number, originY: number) {
 
-        let puckNo = pucks.length - 1;
-        let noOfPucksInRow = 3;
-        let isInc = true;
-        for (let row = 1; row <= 5 && puckNo >= 0; row++) {
-            let gap = 100 - noOfPucksInRow * 20;
-            let x = initialPosX + gap / 2;
-            let y = initialPosY;
+        /*
+        The pattern that we are creating here is like this: path: assets\scripts\puckPattern.png
+        Eg.  (40,45)-->  _______________
+                        |               |    This is (100 x 100) box
+                        |               |
+                        |               |
+                        |               |
+                        |_______________|
+                        
+                        
+        */
+
+
+
+
+        // The below are the cartesion co-ordinates from where our arrangement of pucks will start, or these are the cartesion coordinates of virtual box
+        // in which our pattern will formed. These are the coordinates from upper right corner as shown in above example.
+        let initialPosX = originX;
+        let initialPosY = originY;
+        let initialPosZ = 0;
+
+
+        let sizeOfVirtualBox = 100; // size of virtual box or box that we are imagining in which our pattern will be formed as drawn in above example
+        let widthOfPuckCoinNode = pucks[0].getComponent(UITransform).width;
+
+        let puckIndex = 0; // very first puck is present at index 0 in pucks array
+        let noOfPucksInRow = 3; // It defines no. of puck coins required in ith row, currently it tells that 3 puck coins are required at 0th or 1st row
+        let isNoOfPucksWillIncrease = true; // It is just a flag, which we use to identify wheather we need to increament the noOfPucksInRow variable or decreament
+
+        for (let row = 1; row <= 5; row++) {
+            let gap = sizeOfVirtualBox - (noOfPucksInRow * widthOfPuckCoinNode); // It defines the gap we need to insert from ends in each row
+            let x = initialPosX + (gap / 2); // So we will start inserting the puck coins after leaving the some space (i.e gap variable amount space) from left
             for (let col = 1; col <= noOfPucksInRow; col++) {
-                pucks[puckNo].setPosition(new Vec3(x, y, initialPosZ));
-                x += 22;
-                puckNo--;
+                pucks[puckIndex].setPosition(new Vec3(x, initialPosY, initialPosZ));
+                x += 22; // Here 22 is the amount of spacing that we are maintaining between each coin horizontally
+                puckIndex++;
             }
-            initialPosY -= 20;
+            initialPosY -= 20; // Here 20 is the amount of spacing that we are maintaining between each coin vertically (or also can be said as each row)
 
-            if (noOfPucksInRow >= 5) {
-                isInc = false;
-            }
-            if (isInc) noOfPucksInRow++;
+            if (noOfPucksInRow === 5) isNoOfPucksWillIncrease = false; // If we reach at mid row, it means now our number of pucks will decreament.
+
+            if (isNoOfPucksWillIncrease) noOfPucksInRow++;
             else noOfPucksInRow--;
         }
     }
