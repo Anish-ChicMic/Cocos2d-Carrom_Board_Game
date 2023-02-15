2565ad4c046b8b266d51e86d8065435ebbd58374
ChicMic Technologies <chicmictechnologies@cpe-192-180-0-107.kya.res.rr.com>
2023-02-15 19:12:55 +0530

    Adding green hover and rotation on striker

--- assets/scripts/strikerSliderScript.ts
+++ assets/scripts/strikerSliderScript.ts
@@ -1,11 +1,11 @@
-import { _decorator, Component, Node, Slider, Vec3 } from "cc";
+import { _decorator, Component, Node, Slider, Vec3, Input, input, UITransform, Size, tween} from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("strikerSliderScript")
 export class strikerSliderScript extends Component {
 
     @property(Node)
-    striker: Node | null = null;
+    strikerNode: Node | null = null;
     
 
     
@@ -14,11 +14,32 @@ export class strikerSliderScript extends Component {
     onLoad() {
         // Setting initial position
         this.node.getComponent(Slider).progress = 0.5;
-        let pos = this.striker.getPosition();
-        this.striker.setPosition(0, pos.y, pos.z);
+        let pos = this.strikerNode.getPosition();
+        this.strikerNode.setPosition(0, pos.y, pos.z);
 
+        
+
+        //
+        this.node.on(Input.EventType.MOUSE_DOWN, ()=>{
+            this.strikerNode.children[0].getComponent(UITransform).setContentSize(new Size(40,40));
+            tween(this.strikerNode.children[2])
+            .by(1, {angle: -360})
+            .repeatForever()
+            .start()
+            console.log(this.strikerNode.children.length);
+        }, this)
+        
+        
+        
         // moving striker
         this.node.on('slide', this.moveStriker, this);
+        
+        
+        this.node.on(Input.EventType.MOUSE_UP, ()=>{
+            this.strikerNode.children[0].getComponent(UITransform).setContentSize(new Size(80,80));
+            this.strikerNode.children[2].active = false;
+            console.log("")
+        }, this)
     }
 
     start() {}
@@ -30,7 +51,7 @@ export class strikerSliderScript extends Component {
 
         let progress = this.node.getComponent(Slider).progress;
         progress = progress * this.width;
-        let pos = this.striker.getPosition();
-        this.striker.setPosition(new Vec3(this.initialPos + progress, pos.y, pos.z));
+        let pos = this.strikerNode.getPosition();
+        this.strikerNode.setPosition(new Vec3(this.initialPos + progress, pos.y, pos.z));
     }
 }
